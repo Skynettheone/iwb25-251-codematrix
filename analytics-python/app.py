c265053173e961d4905672b90f370420e36eaa33
S3thanaut <siyath.8595@gmail.com>
2025-08-13 23:03:08 +0530

    Changing from Flask to FastAPI

--- analytics-python/app.py
+++ analytics-python/app.py
@@ -1,15 +1,16 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/status", methods=["GET"])
-def get_status():
+@app.get("/status")
+async def get_status():
     """Returns the operational status of the analytics service."""
     print("Request received for analytics status check.")
     response = {
         "status": "Python analytics service is running successfully"
     }
-    return jsonify(response)
+    return response
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
